3d9d71ecb2aadbc7cfe8037c874fc91a7c88d4ed
Brayan <brayancreepy25@gmail.com>
2021-01-12 13:06:58 -0500

    cambios

---
+++ MySQL/academia_bd.sql
@@ -0,0 +1,6 @@
+USE academia;
+
+CREATE TABLE tipo_documento(
+    codigo VARCHAR(3) PRIMARY KEY,
+    descripción VARCHAR(100) NOT NULL
+);
---
+++ MySQL/generales.sql
@@ -0,0 +1,5 @@
+-- mysql -h localhost -u root -p
+SHOW databases; --Visualizar las bases de datos en MySQL
+SHOW tables; --visualizar las tablas
+USE academia;
+CREATE USER ''@'localhost' IDENTIFIED BY 'password'
